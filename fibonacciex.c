523f0dbc127d87f2b4764b6a535a58aa8def155d
PilakaPreddy <89542999+PilakaPreddy@users.noreply.github.com>
2022-01-19 21:13:40 +0530

    Add files via upload

---
+++ fibonacciex.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,n,i;
+	printf("enter the numner to print fibonacci\n");
+	scanf("%d",&n);
+	
+
+	for(i=0;i<=n;i++)
+{
+	printf("%d\n", a);
+	c= a+b;
+	a= b;
+	b= c;
+}
+}
---
+++ patterneexample.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int r,c,i,j;
+	printf("enter the no of rows\n");
+	scanf("%d",&r);
+	printf("enter the no of columns\n");
+	scanf("%d",&c);
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=5;j++)
+		{
+			
+			
+			printf("i");
+			
+		}
+		printf("\n");
+	}
+}
